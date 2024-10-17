db809885cb79c2c32d52c17bba12365017f80664
OnkarJondhale <onkar.jondhale@gmail.com>
2024-10-18 03:48:43 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        rotate: 'rotate 1s linear 1',
+        rotaterev : 'rotaterev 1s linear 1',
+      },
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        rotaterev: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(-360deg)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
